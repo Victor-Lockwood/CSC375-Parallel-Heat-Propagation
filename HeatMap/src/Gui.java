18e04a64aa8fab69d2d3e6f1cc1dbfae508f37b0
Victor-Lockwood <vlockwoo@oswego.edu>
2023-11-29 22:10:19 -0500

    Neat GUI colors

--- HeatMap/src/Gui.java
+++ HeatMap/src/Gui.java
@@ -31,22 +31,62 @@ public class Gui extends JPanel {
             for(int j = 0; j < cellLine.length; j++) {
                 int currentX = startX + (blockLength * j);
 
-                if(cellLine[j].temperature < 20) {
-                    //Cold
-                    graphics.setColor(Color.blue);
-                } else if(cellLine[j].temperature < 40) {
-                    //Warm
-                    graphics.setColor(Color.yellow);
-                } else if(cellLine[j].temperature < 60) {
-                    //Hot
-                    graphics.setColor(Color.orange);
-                } else if(cellLine[j].temperature < 80) {
-                    //Hotter
-                    graphics.setColor(Color.red);
+//                if(cellLine[j].temperature < 20) {
+//                    //Cold
+//                    graphics.setColor(Color.blue);
+//                } else if(cellLine[j].temperature < 40) {
+//                    //Warm
+//                    graphics.setColor(Color.yellow);
+//                } else if(cellLine[j].temperature < 60) {
+//                    //Hot
+//                    graphics.setColor(Color.orange);
+//                } else if(cellLine[j].temperature < 80) {
+//                    //Hotter
+//                    graphics.setColor(Color.red);
+//                } else {
+//                    //Hottest
+//                    graphics.setColor(Color.magenta);
+//                }
+
+                //Kelvin = Celsius + 273.15
+
+                //Ported code from here: https://gist.github.com/paulkaplan/5184275
+                double modifiedTemp = (cellLine[j].temperature + 273.15) / 100;
+
+                int red, blue, green;
+
+                if(modifiedTemp <= 66) {
+                    red = 255;
+                    green = (int)modifiedTemp;
+                    green = (int)(99.4708025861 * Math.log(green) - 161.1195681661);
+
+                    if(modifiedTemp <= 19) {
+                        blue = 0;
+                    } else {
+                        blue = (int)(modifiedTemp - 10);
+                        blue = (int)(138.5177312231 * Math.log(blue) - 305.0447927307);
+                    }
                 } else {
-                    //Hottest
-                    graphics.setColor(Color.magenta);
+                    red = (int)modifiedTemp - 60;
+                    red = (int)(329.698727446 * Math.pow(red, -0.1332047592));
+
+                    green = (int)modifiedTemp - 60;
+                    green = (int)(288.1221695283 * Math.pow(green, -0.0755148492 ));
+
+                    blue = 255;
                 }
+
+                if(red < 0) red = 0;
+                if(red > 255) red = 255;
+
+                if(green < 0) green = 0;
+                if(green > 255) green = 255;
+
+                if(blue < 0) blue = 0;
+                if(blue > 255) blue = 255;
+
+                Color tempColor = new Color(red, green, blue);
+                graphics.setColor(tempColor);
 
                 graphics.fillRect(currentX, currentY, blockLength, blockLength);
             }
